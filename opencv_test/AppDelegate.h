5e075518ca8c512ae90b1d68883593a5a318fb06
misra_shivang <misra_shivang@bitbucket.org>
2017-02-17 13:09:25 +0530

    Initial Commit

---
+++ opencv_test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  opencv_test
+//
+//  Created by Jasvir Singh on 17/02/17.
+//  Copyright © 2017 Macrew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ opencv_test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  opencv_test
+//
+//  Created by Jasvir Singh on 17/02/17.
+//  Copyright © 2017 Macrew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
